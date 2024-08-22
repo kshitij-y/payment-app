5352b58e732d79d68f9cb499e70f0e2eff02e255
Kshitij Kumar <119648768+kshitij-y@users.noreply.github.com>
2024-08-23 04:31:00 +0530

    Update index.js  cors

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://paytm-project-app-ksh.vercel.app"],
+    methods: ["POST", "GET", "PUT", "UPDATE"],
+    credentials: true
+}));
 app.use(express.json());
 
 const mainRouter = require("./routes/index")
