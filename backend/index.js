095363bc891b1a20906da255cb0c03d75361ad2d
kshit <kshit>
2024-08-25 02:51:00 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -8,5 +8,5 @@ app.use(express.json());
 const mainRouter = require("./routes/index")
 app.use("/api/v1", mainRouter);
 
-
-app.listen(3000)
+const port = process.env.PORT || 3000 ;
+app.listen(port)
