41f66f96b04bdf70b5c06f8542a4c7d607741332
Kshitij Kumar <119648768+kshitij-y@users.noreply.github.com>
2024-09-06 04:32:04 +0530

    aws

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
